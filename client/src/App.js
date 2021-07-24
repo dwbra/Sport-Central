d13533fada55d6c8222275ba117ef80b9952620b
brianskiies <brian.he3@gmail.com>
2021-07-24 14:42:22 +1000

    updated routers and components structure

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import {useDispatch } from 'react-redux'
 import { getAds } from './actions/ads.js'
 import Ads from './components/Ads/Ads.js'
 import Form from './components/Form/Form.js'
+import Home from './components/Home/Home.js'
 
 function App() {
   const [currentId, setCurrentId] = useState(null)
@@ -24,30 +25,19 @@ function App() {
       <Container maxWidth="lg">
       <Typography variant="h2" align="center">Sport Central</Typography>
       <Router>
-          <ul>
-            <li><Link to="/create">Create Ad</Link></li>
-            <li><Link to="/explore">Explore</Link></li>
-          </ul>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/create">Create Ad</Link></li>
+          <li><Link to="/explore">Explore</Link></li>
+        </ul>
         <Switch>
-          <Route path="/create" component={() => <Form currentId={currentId} setCurrentId={setCurrentId} />}/>
-          <Route path="/explore" component={() => <Ads setCurrentId={setCurrentId} />}/>
+          <Route path="/" exact component={() => <Home />}/>
+          <Route path="/create" exact component={() => <Form currentId={currentId} setCurrentId={setCurrentId} />}/>
+          <Route path="/explore" exact component={() => <Ads setCurrentId={setCurrentId} />}/>
         </Switch>
       </Router>
         <AppBar position="static" color="inherit">
-
         </AppBar>
-        {/* <Grow in>
-        <Container>
-          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-            <Grid item xs={12} sm={7}>
-              <Ads setCurrentId={setCurrentId} />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId} />
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow> */}
     </Container>
     </div>
   );
