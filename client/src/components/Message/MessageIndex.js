9c10311e387802fb381d87d954d379085b8072d5
“dwbra” <dworkmang@gmail.com>
2021-07-31 12:46:30 +1000

    added socket, not connecting to backend properly and broke frontend

--- client/src/components/Message/MessageIndex.js
+++ client/src/components/Message/MessageIndex.js
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react';
 import Dashboard from './Dashboard';
 import { ContactsProvider } from './Providers/ContactsProvider';
 import { ConversationsProvider } from './Providers/ConversationsProvider';
+import SocketProvider from './Providers/SocketProvider';
 
 function MessageIndex() {
 
@@ -14,12 +15,13 @@ function MessageIndex() {
     const id = userEmail
 
     const dashboard = (
-        
+        <SocketProvider id={id}>
         <ContactsProvider>
             <ConversationsProvider id={id}>
                 <Dashboard id={id} />
             </ConversationsProvider>
         </ContactsProvider>
+        </SocketProvider>
     )
 
     return (
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { Server } from "socket.io";
 
 import adRoutes from './routes/ads.js';
 import authRoutes from './routes/auth.js';
@@ -28,8 +29,26 @@ app.use('/sports', sportRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGODB, {useNewUrlParser: true, useUnifiedTopology: true })
+const primaryServer = mongoose.connect(process.env.MONGODB, {useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
 mongoose.set('useFindAndModify', false);
+
+const io = new Server(primaryServer, {
+});
+
+io.on("connection", (socket) => {
+    const id = socket.handshake.query.id
+    socket.join(id)
+
+    socket.on('send-message', ({ recipients, text }) => {
+        recipients,forEach(recipient => {
+            const newRecipients = recipients.filter(r => r !== recipient)
+            newRecipients.push(id)
+            socket.broadcast.to(recipient).emit('receive-message', {
+                recipients: newRecipients, sender: id, text
+            })
+        })
+    })
+})
